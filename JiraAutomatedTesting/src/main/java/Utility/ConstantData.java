5583de0263d0843dee75d52289b8f52f04f6dfc5
SzervetG <90453505+SzervetG@users.noreply.github.com>
2022-11-06 09:46:00 +0100

    Add constant data template

---
+++ JiraAutomatedTesting/src/main/java/Utility/ConstantData.java
@@ -0,0 +1,9 @@
+package Utility;
+
+public class ConstantData {
+
+    public static final String jiraUser = null;
+    public static final String jiraPass = null;
+    public static final String baseUrl = null;
+
+}
